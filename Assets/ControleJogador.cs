b43888c9cc58f4797769436e72e9653eec125639
Felipe Vieira Dantas <felipe@gmfgames.com>
2022-08-23 12:06:03 -0300

    Add files via upload

--- Assets/ControleJogador.cs
+++ Assets/ControleJogador.cs
@@ -7,6 +7,9 @@ public class ControleJogador : MonoBehaviour
     Rigidbody fisica;
     public float velocidade = 10;
     public float forcaPulo = 300;
+    public float velocidadeGira = 5;
+
+    public bool estaNoChao;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +27,58 @@ public class ControleJogador : MonoBehaviour
         //Mostra uma mensagem só pra gente, no Console.
         Debug.Log(movimentoX);
 
-        fisica.velocity = new Vector3(
+        //Movimento normal considerando X e Z da Unity
+        /*fisica.velocity = new Vector3(
             movimentoX*velocidade,
             fisica.velocity.y,
-            movimentoY * velocidade);
+            movimentoY * velocidade);*/
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        transform.Rotate(0, movimentoX * velocidadeGira, 0);
+        //Se eu quiser que ele rode sempre 90 graus:
+        /*if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(0, 90, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(0, -90, 0);
+        }*/
+
+        Vector3 direcao = transform.forward * movimentoY;
+        direcao = direcao * velocidade;
+        direcao.y = fisica.velocity.y;
+
+        fisica.velocity = direcao;
+
+        if (Input.GetKeyDown(KeyCode.Space) && estaNoChao) {
             fisica.AddForce(0, forcaPulo, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            estaNoChao = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            estaNoChao = false;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            other.transform.parent = transform;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+        }else
+        {
+            other.transform.parent = null;
+            other.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 }
