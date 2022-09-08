89aa579c3a431eb24d84440ae3945d1aa19868e9
Felipe Vieira Dantas <felipe@gmfgames.com>
2022-09-08 13:28:50 -0300

    Add files via upload

--- Assets/Animações/MovimentoBayonetta.cs
+++ Assets/Animações/MovimentoBayonetta.cs
@@ -6,12 +6,42 @@ public class MovimentoBayonetta : MonoBehaviour
 {
     public float velocidade = 10;
     Rigidbody fisica;
+
+    Animator anim;
+    bool atacando;
+
     void Start()
     {
         fisica = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
+        if (atacando)
+        {
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsTag("Porrada"))
+            {
+                atacando = false;
+            }
+        }
+
+        //Atacar com o botão esquerdo do mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsTag("Porrada"))
+            {
+                anim.SetTrigger("Ataque");
+                atacando = true;
+                anim.ResetTrigger("Combo");
+            }
+            else
+            {
+                anim.SetTrigger("Combo");
+            }
+        }
+
+
+
         float movimentoX = Input.GetAxis("Horizontal");
         float movimentoZ = Input.GetAxis("Vertical");
         Vector3 movimento = new Vector3(movimentoX, 0, movimentoZ);
@@ -22,5 +52,15 @@ public class MovimentoBayonetta : MonoBehaviour
         movimento.y = 0;
         transform.rotation = Quaternion.LookRotation(movimento);
         fisica.velocity = movimento * velocidade;
+
+        //Ligar ou desligar animação de andando
+        if(movimentoX != 0 || movimentoZ != 0)
+        {
+            anim.SetBool("Andando", true);
+        }
+        else
+        {
+            anim.SetBool("Andando", false);
+        }
     }
 }
